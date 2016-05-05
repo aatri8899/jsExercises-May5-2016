c02e948b7c3b4b60ed70af4848b7fb7a28360d15
aatri8899 <aatri8899@withs.org>
2016-05-05 14:55:48 +0000

    jsExercises May-5-2016

--- for-loop-exercises.js
+++ for-loop-exercises.js
@@ -25,7 +25,11 @@ function example() {
   Inside the body, create a for loop that repeats 5 times
   Inside the for loop, call run()
 */
-
+function looper(){
+  for (var i=0; i<5; i++){
+  run();
+  }
+}
 
 /*
   Create a function named 'inception' with one parameter
@@ -38,7 +42,11 @@ function example() {
     inception(4) -> run() is called four times
     inception(0) -> run() should not be called
 */
-
+function inception(loop){
+  for (var i = 0; i < loop; i++){
+    run();
+  }
+}
 
 /*
   Create a function named 'redLightGreenLight' with two parameters
@@ -51,8 +59,17 @@ function example() {
     redLightGreenLight(4, true) -> run() is called four times
     redLightGreenLight(0, false) -> stop() should not be called
 */
+function redLightGreenLight(blue, purple){
+  for (var i = 0; i < blue; i++){
 
-
+    if (purple){
+       run();
+    }
+    else {
+       stop();
+    }
+}
+}
 /*
   Create a function named 'range' with two parameters
   Name the parameters anything you want
@@ -66,7 +83,11 @@ function example() {
     range(3, 6) -> run() is called thrice
     range(2, 0) -> run() should not be called
 */
-
+function range(positive, negative){
+  for (var i = positive; i < negative; i++){
+    run();
+  }
+}
 
 /*
   Create a function named 'rangeAdv' with three parameters
@@ -84,3 +105,8 @@ function example() {
     rangeAdv(2, 0, 5) -> run() should not be called
     rangeAdv(0, 2, 5) -> run() is called once
 */
+function rangeAdv(me, myself, I){
+  for (var i = me; i<I ; i++){
+    run()
+  }
+}
